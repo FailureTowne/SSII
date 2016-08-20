33dd28003150e92f5aaa700251794e1de2f85f23
Katie Peavey <Katie Peavey>
2016-08-20 12:29:37 -0700

    CAN YOU TELL I'M FLAILING HERE???

---
+++ SSII/Assets/RotateInput.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateInput : MonoBehaviour {
+
+    public float Acceleration { get; set; }
+    public float MaxSpeed { get; set; }
+    private double Speed { get; set; }
+    private float InitialRotation { get; set; }
+    private double PreviousRotation { get; set; }
+
+    void Start () {
+        Acceleration = 20;
+        MaxSpeed = 1000;
+        InitialRotation = this.gameObject.transform.rotation.eulerAngles.y;
+	}
+	
+	void Update () {
+        //passive deceleration
+        if(Speed != 0)
+        {
+            Speed = (.9 * Speed);
+        }
+  
+        //rotate left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Speed += Time.deltaTime * Acceleration;
+        }
+
+        //rotate right
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Speed -= Time.deltaTime * Acceleration;
+        }
+
+        //cap speeds
+        if(Speed > MaxSpeed)
+        {
+            Speed = MaxSpeed;
+        }
+        else if(Speed < -MaxSpeed)
+        {
+            Speed = -MaxSpeed;
+        }
+
+        //do trasnform
+        transform.rotation = Quaternion.Euler(0, InitialRotation, transform.rotation.x + (float)Speed);
+    }
+}
