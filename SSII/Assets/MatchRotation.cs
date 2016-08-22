f07da61a7eb39510ce5caa8d83bc120827148a10
Connor <FailureTowne@users.noreply.github.com>
2016-08-21 22:42:08 -0700

    Enemy spawining working correctly-ish More to come on handling waves

--- SSII/Assets/MatchRotation.cs
+++ SSII/Assets/MatchRotation.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class MatchRotation : MonoBehaviour {
 
-	// Use this for initialization
+    // Use this for initialization
+    public Transform RotateMimick;
 	void Start () {
-        transform.localRotation = Quaternion.identity;
+        gameObject.transform.rotation = RotateMimick.rotation;
+        gameObject.transform.Rotate(new Vector3(0, 0, 90));
     }
 	
 	// Update is called once per frame
