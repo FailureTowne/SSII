367c37729130659bc76ed422e093030561fc0e0b
Mike Homnick <mike.homnick@gmail.com>
2016-08-25 10:10:03 -0700

    Pulled in legacy atmo and played with lighting a bit

---
+++ SSII/Assets/MonoLight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonoLight : MonoBehaviour {
+
+    public Light AtmoLight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        AtmoLight.intensity = Mathf.PingPong(Time.time * 5, 8);
+
+	}
+}
