77aeb4dab461560e884040c156600ec23bd7c4cc
Mike Homnick <mike.homnick@gmail.com>
2016-08-25 18:38:21 -0700

    Added asteroids and they kind of work

---
+++ SSII/Assets/AsteroidRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidRotate : MonoBehaviour {
+
+    public int AsteroidRotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.right, Time.deltaTime * AsteroidRotateSpeed);
+	
+	}
+}
