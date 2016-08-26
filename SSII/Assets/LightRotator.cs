03003fb61f58c507390a627493a8ce2c2cc3b380
Mike Homnick <mike.homnick@gmail.com>
2016-08-25 21:51:56 -0700

    Lighting fun

---
+++ SSII/Assets/LightRotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightRotator : MonoBehaviour {
+
+    public int RotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.down * Time.deltaTime * RotateSpeed);
+
+	}
+}
