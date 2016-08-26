7cf616853c44a2433ded166ff5735d68019aadce
Connor <FailureTowne@users.noreply.github.com>
2016-08-25 21:54:43 -0700

    Rudementary atmo collision

---
+++ SSII/Assets/AtmoCollide.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtmoCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+
+    void OnCollisionEnter(Collision atmo)
+    {
+        System.Console.WriteLine("IN COLLISION");
+        if (atmo.gameObject.tag == "atmo")
+        {
+            GetComponent<JellyfishMovement>().Speed = 0;
+        }
+    }
+}
---
+++ SSII/Assets/AtmoHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtmoHealth : MonoBehaviour {
+
+    public float RateOfLoss;
+	// Use this for initialization
+	void Start () {
+        RateOfLoss = 0;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector3(transform.localScale.x - RateOfLoss, transform.localScale.y - RateOfLoss, transform.localScale.z - RateOfLoss);
+    }
+	
+    void OnCollisionEnter(Collision col)
+    {
+        col.gameObject.GetComponent<JellyfishMovement>().Speed = 0;
+        RateOfLoss += .001F;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        col.gameObject.GetComponent<JellyfishMovement>().Speed = .1F;
+        RateOfLoss -= .001F;
+    }
+}
