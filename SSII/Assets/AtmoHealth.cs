2a297c79929844209201e15d5c9a28997e51ab2c
Connor <FailureTowne@users.noreply.github.com>
2016-08-26 01:02:14 -0700

    I swear this was working before. Things seem to be okay with git now. Lighting looks sweet!

--- SSII/Assets/AtmoHealth.cs
+++ SSII/Assets/AtmoHealth.cs
@@ -3,27 +3,28 @@ using System.Collections;
 
 public class AtmoHealth : MonoBehaviour {
 
+    public float TotalRateOfLoss;
     public float RateOfLoss;
 	// Use this for initialization
 	void Start () {
-        RateOfLoss = 0;
+        TotalRateOfLoss = 0;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        transform.localScale = new Vector3(transform.localScale.x - RateOfLoss, transform.localScale.y - RateOfLoss, transform.localScale.z - RateOfLoss);
+        transform.localScale = new Vector3(transform.localScale.x - TotalRateOfLoss, transform.localScale.y - TotalRateOfLoss, transform.localScale.z - TotalRateOfLoss);
     }
 	
     void OnCollisionEnter(Collision col)
     {
         col.gameObject.GetComponent<JellyfishMovement>().Speed = 0;
-        RateOfLoss += .001F;
+        TotalRateOfLoss += RateOfLoss;
     }
 
     void OnCollisionExit(Collision col)
     {
-        col.gameObject.GetComponent<JellyfishMovement>().Speed = .1F;
-        RateOfLoss -= .001F;
+        col.gameObject.GetComponent<JellyfishMovement>().Speed = .05F;
+        TotalRateOfLoss -= RateOfLoss;
     }
 }
