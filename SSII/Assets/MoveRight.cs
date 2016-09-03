415a13bacbbd3a026d231838919c2ea75d9051c9
Connor Peavey <peaveyc@onid.oregonstate.edu>
2016-09-03 00:14:19 -0700

    refactoring, fixed background particle systems

---
+++ SSII/Assets/MoveRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRight : MonoBehaviour {
+
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.right * Time.deltaTime * Speed);
+
+	}
+}
---
+++ SSII/Assets/RotateRight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateRight : MonoBehaviour {
+
+    public int RotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(Vector3.right, Time.deltaTime * RotateSpeed);
+	
+	}
+}
